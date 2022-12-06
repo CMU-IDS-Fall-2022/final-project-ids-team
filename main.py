c9ab3a2ba65f2027dcd32a421fb12989574889c3
Nicola115 <2225992901@qq.com>
2022-12-05 20:13:28 -0500

    normalize
    Signed-off-by: Nicola115 <2225992901@qq.com>

--- main.py
+++ main.py
@@ -74,8 +74,9 @@ with cols[0]:
     selections = np.array(['Year','Beats Per Minute (BPM)',	'Energy',	'Danceability',	'Loudness (dB)'	,'Liveness',	'Valence','Length (Duration)','Acousticness','Speechiness',	'Popularity'])
     simi_feature=st.selectbox('Interested feature',selections)
 
-diff = df[simi_feature] - df[simi_feature][:,None]    
-diff_matrix = pd.concat((df['Title'], pd.DataFrame(diff, columns=df['Title'])), axis=1)
+diff = df[simi_feature].values - df[simi_feature].values[:,None]
+diff_norm = (diff-diff.min())/(diff.max()-diff.min())*100    
+diff_matrix = pd.concat((df['Title'], pd.DataFrame(diff_norm, columns=df['Title'])), axis=1)
 
 st.write("Here is the recommendation network based on your selection.")
 
