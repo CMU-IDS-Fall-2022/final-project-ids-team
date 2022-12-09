04cabac51bb83adae3ed1857af253c1692651c17
Tanvi Karandikar <tanvi.karandikar141@gmail.com>
2022-12-08 23:00:40 -0500

    logos

--- app/Home.py
+++ app/Home.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from PIL import Image
+import requests
+from io import BytesIO
 
-image1 = Image.open('./data/billboard.jpg')
-image2 = image1
+response = requests.get("https://github.com/CMU-IDS-Fall-2022/final-project-ids-team/blob/main/app/data/spotify.png?raw=true")
+image1 = Image.open(BytesIO(response.content))
+# image1 = Image.open('./data/billboard.jpg')
+response = requests.get("https://github.com/CMU-IDS-Fall-2022/final-project-ids-team/blob/main/app/data/billboard.jpg?raw=true")
+image2 = Image.open(BytesIO(response.content))
 # image2 = Image.open('https://github.com/CMU-IDS-Fall-2022/final-project-ids-team/blob/main/app/data/spotify.png?raw=true')
 
 page_bg = """
